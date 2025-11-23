8f4f11a0039f8f23d95d961dedca90e10658720e
Muhammad Sami <msamiaj@outlook.com>
2025-11-23 20:23:36 +0500

    main.ts: added NestLogger 🚀

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger as NestLogger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api/v1');
-  await app.listen(process.env.PORT ?? 3000);
+  const globalPrefix = 'api/v1';
+  const port = process.env.PORT ?? 3000;
+
+  app.setGlobalPrefix(globalPrefix, { exclude: ['{/*splat}'] });
+  await app.listen(port);
+  NestLogger.log(`🚀 Application running on http://127.0.0.1:${port}/${globalPrefix}`);
 }
 bootstrap();
