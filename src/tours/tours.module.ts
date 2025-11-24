69deed2995ce7fb2f1a683c47374d7b2d995e8a1
Muhammad Sami <msamiaj@outlook.com>
2025-11-25 01:04:22 +0500

    tours.module.ts: added tour model import 💪

--- src/tours/tours.module.ts
+++ src/tours/tours.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ToursController } from './tours.controller';
 import { ToursService } from './tours.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Tour, TourSchema } from './schemas/tour.schema';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: Tour.name, schema: TourSchema }])],
   controllers: [ToursController],
-  providers: [ToursService]
+  providers: [ToursService],
 })
 export class ToursModule {}
