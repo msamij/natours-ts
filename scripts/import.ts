42b92bcbf1f9881b6b38c9d363e27a924f381fc0
Muhammad Sami <msamiaj@outlook.com>
2025-11-25 01:19:35 +0500

    traveling to commit 69deed2

--- scripts/import.ts
+++ scripts/import.ts
@@ -1,45 +1,45 @@
-import '../config.js';
-import fs from 'fs';
-import path from 'path';
-import mongoose from 'mongoose';
-import Tour from '../models/tourModel.js';
-import { getEnvVar } from '../config.js';
-import { logger } from '../logger.js';
+// import '../config.js';
+// import fs from 'fs';
+// import path from 'path';
+// import mongoose from 'mongoose';
+// import Tour from '../models/tourModel.js';
+// import { getEnvVar } from '../config.js';
+// import { logger } from '../logger.js';
 
-const DB = getEnvVar('DATABASE').replace('<PASSWORD>', getEnvVar('DATABASE_PASSWORD'));
+// const DB = getEnvVar('DATABASE').replace('<PASSWORD>', getEnvVar('DATABASE_PASSWORD'));
 
-mongoose.connect(DB).then(() => {
-  logger.info('DB connection succesfull');
-});
+// mongoose.connect(DB).then(() => {
+//   logger.info('DB connection succesfull');
+// });
 
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours.json`, 'utf-8'));
+// const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours.json`, 'utf-8'));
 
-const importData = async () => {
-  try {
-    await Tour.create(tours);
-    logger.info('Data loaded successfully!');
-  } catch (err) {
-    logger.error(`An error occured wrong while loading data: ${err}`);
-  } finally {
-    process.exit();
-  }
-};
+// const importData = async () => {
+//   try {
+//     await Tour.create(tours);
+//     logger.info('Data loaded successfully!');
+//   } catch (err) {
+//     logger.error(`An error occured wrong while loading data: ${err}`);
+//   } finally {
+//     process.exit();
+//   }
+// };
 
-const deleteData = async () => {
-  try {
-    await Tour.deleteMany();
-    logger.info('Data deleted successfully!');
-  } catch (err) {
-    logger.error(`An error occured wrong while deleting data: ${err}`);
-  } finally {
-    process.exit();
-  }
-};
+// const deleteData = async () => {
+//   try {
+//     await Tour.deleteMany();
+//     logger.info('Data deleted successfully!');
+//   } catch (err) {
+//     logger.error(`An error occured wrong while deleting data: ${err}`);
+//   } finally {
+//     process.exit();
+//   }
+// };
 
-if (process.argv[2] === '--import') {
-  importData();
-} else if (process.argv[2] === '--delete') {
-  deleteData();
-}
+// if (process.argv[2] === '--import') {
+//   importData();
+// } else if (process.argv[2] === '--delete') {
+//   deleteData();
+// }
